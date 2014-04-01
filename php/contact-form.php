b5e18e51db7efc8d31b81b2bf433b5df940ef1fc
amannanov <mr.mannanov@gmail.com>
2014-04-01 18:23:28 +0400

    Block beta 2

---
+++ php/contact-form.php
@@ -0,0 +1,58 @@
+<?php
+session_cache_limiter('nocache');
+header('Expires: ' . gmdate('r', 0));
+
+header('Content-type: application/json');
+
+// Enter your email address below.
+$to = 'mr.mannanov@gmail.com';
+
+if($to) {
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$tel = $_POST['message'];
+
+	$subject = "test";
+
+	$fields = array(
+		0 => array(
+			'text' => 'Name',
+			'val' => $_POST['name']
+		),
+		1 => array(
+			'text' => 'Email address',
+			'val' => $_POST['email']
+		),
+		2 => array(
+			'text' => 'Phone number',
+			'val' => $_POST['message']
+		)
+	);
+
+	$message = "";
+
+	foreach($fields as $field) {
+		$message .= $field['text'].": " . htmlspecialchars($field['val'], ENT_QUOTES) . "<br>\n";
+	}
+
+	$headers = '';
+	$headers .= 'From: ' . $name . ' <' . $email . '>' . "\r\n";
+	$headers .= "Reply-To: " .  $email . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+	if (mail($to, $subject, $message, $headers)){
+		$arrResult = array ('response'=>'success');
+	} else{
+		$arrResult = array ('response'=>'error');
+	}
+
+	echo json_encode($arrResult);
+
+} else {
+
+	$arrResult = array ('response'=>'error');
+	echo json_encode($arrResult);
+
+}
+?>
